7f6693d695fa57de504ebf84a12162f6c47fe7d3
Ricardo Sutil <ricardosutil@gmail.com>
2021-08-24 22:07:47 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/main/java/com/sutil/ricardo/hreurekaserver/HrPayroll1Application.java
@@ -0,0 +1,13 @@
+package com.sutil.ricardo.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayroll1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayroll1Application.class, args);
+	}
+
+}
---
+++ hr-eureka-server/src/test/java/com/sutil/ricardo/hreurekaserver/HrPayroll1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutil.ricardo.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayroll1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
